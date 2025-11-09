e3ffe712be64dca3fedc0def299308c70942ea33
iriando <taufik.iriando@gmail.com>
2025-11-09 22:05:25 +0900

    activated edit profil filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login(Login::class)
+            ->profile()
             ->colors([
                 'primary' => Color::Amber,
             ])
